bb53cf4ddce458f03aa9a942be04f05126641913
Helen <helendiaz.1340@gamil.com>
2025-09-01 18:05:07 -0500

    Proyecto Angular inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Facturacion } from './facturacion/facturacion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'facturas', component: Facturacion },
+  // Puedes añadir más rutas aquí para otros componentes
+];
